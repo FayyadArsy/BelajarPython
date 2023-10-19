29a5bcc87b26d7a5a47dede244b1e2063cbd1858
FayyadArsy <fayyadarsynandi2@gmail.com>
2023-10-19 14:41:41 +0700

    add CRUD endpoint

--- day3/app/__init__.py
+++ day3/app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_restx import Api
+from flask_migrate import Migrate
+# from flask_sqlalchemy import SQLAlchemy
 from .config.config import config_dict
 from .views.users import users_ns
+from .utils import db
+from .models import users
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
     app.config.from_object(config)
     
+    db.init_app(app)
     api = Api(
         app,
         doc='/docs',
@@ -15,5 +20,6 @@ def create_app(config=config_dict['dev']):
     )
 
     api.add_namespace(users_ns)
-    
+    # api.add_namespace(users_ns)
+    migrate = Migrate(app, db)
     return app
--- day3/app/config/config.py
+++ day3/app/config/config.py
@@ -6,8 +6,11 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 class Config:
     SECRET_KEY = config('SECRET_KEY','secret')
     DEBUG = config('DEBUG', cast=bool)
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+    SQLALCHEMY_ECHO = config('SQLALCHEMY_ECHO', cast=bool)
 class Devconfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:''@localhost/latihanapi'
+    SQLALCHEMY_RECORD_QUERIES = config('SQLALCHEMY_RECORD_QUERIES', cast=bool)
 class Qasconfig(Config):
     pass
 class Prdconfig(Config):
